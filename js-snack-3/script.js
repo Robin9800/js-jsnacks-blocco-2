c866ac8c11ef975a70e6fc0f49725a532ffe4d58
Robin9800 <robin.costanzo@gmail.com>
2022-03-15 01:05:49 +0100

    exercise 3

---
+++ js-snack-3/script.js
@@ -0,0 +1,25 @@
+/*Crea due array che hanno un numero di elementi diversi.
+ Aggiungi elementi casuali all’array che ha meno elementi, fino a quando ne avrà tanti quanti l’altro.*/
+
+const array1 = [2, 3, 'peppe',  8, 12, 5] ;
+
+const array2 = [3, 'robin', 6, 2,];
+
+let minore2;
+
+if (array1.length < array2.length){
+  console.log('array1 < array2');
+  minore2 = array1;
+
+} else {
+  console.log('array2 < o = a array1');
+  minore2 = array2;
+
+} 
+  
+
+while(array1.length !== array2.length){
+
+  minore2.push('X');
+
+}
